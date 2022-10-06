326d067f38fdebc0456554a3b845074b4ed7fe6e
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2022-10-06 20:18:26 +0300

    3st com

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,6 @@
 // Add imports above this line
 import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
-// Change code below this line
+// Change code below this lin
 console.log(SimpleLightbox);
 console.log(galleryItems);
