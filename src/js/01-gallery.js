32387082e5bd0a8fdee85b8db141522dcac0c48e
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2022-10-07 18:51:40 +0300

    2nd task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,24 @@
 // Add imports above this line
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this lin
-console.log(SimpleLightbox);
-console.log(galleryItems);
+// Change code below this line
+const galleryBox = document.querySelector('.gallery');
+
+createGallery(galleryItems, galleryBox);
+
+function createGallery(galleryItems, galleryBox) {
+  const galleryLi = galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+  galleryBox.innerHTML = galleryLi;
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
