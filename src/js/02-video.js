636d506f79da2df5b4590837310b342db5a67ba9
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2022-10-08 23:07:41 +0300

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,11 +2,12 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay(data) {
   localStorage.setItem('videoplayer-current-time', data.seconds);
 }
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
